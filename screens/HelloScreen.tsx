f7bb5a48c37ec0a753b6803852a924e38b86fac8
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-09-20 17:03:45 +0900

    memo + useCallback

--- screens/HelloScreen.tsx
+++ screens/HelloScreen.tsx
@@ -1,20 +1,30 @@
 import { View, Text } from 'react-native';
-import React, { useEffect, VFC } from 'react';
+import React, { useCallback, useEffect, useState, VFC } from 'react';
 import tw from 'tailwind-rn';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Button } from 'react-native-elements';
+import { Button, Input } from 'react-native-elements';
+import { FontAwesome } from '@expo/vector-icons';
 
 import { RootStackParamList } from '../types/types';
+import { Child } from '../components/Child';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Hello'>;
 
 export const HelloScreen: VFC<Props> = ({ navigation }) => {
+  const [text, setText] = useState('');
+  const [printText, setPrintText] = useState('');
+
   useEffect(() => {
     console.log('mounted Hello');
     return () => {
       console.log('unmounted Hello');
     };
   }, []);
+
+  const printMsg = useCallback(() => {
+    console.log(`Print: ${printText}`);
+  }, [printText]);
+
   return (
     <View style={tw('flex-1 bg-gray-300 justify-center items-center')}>
       <Text>Hello</Text>
@@ -24,6 +34,21 @@ export const HelloScreen: VFC<Props> = ({ navigation }) => {
           onPress={() => navigation.navigate('ReduxTK')}
         />
       </View>
+      <Input
+        placeholder="type print text"
+        leftIcon={<FontAwesome name="pencil" size={24} color="gray" />}
+        value={text}
+        onChangeText={(txt) => setText(txt)}
+      />
+      <Text>{text}</Text>
+      <Input
+        placeholder="type print text"
+        leftIcon={<FontAwesome name="pencil" size={24} color="gray" />}
+        value={printText}
+        onChangeText={(txt) => setPrintText(txt)}
+      />
+      <Text>{printText}</Text>
+      <Child printMsg={printMsg} />
     </View>
   );
 };
